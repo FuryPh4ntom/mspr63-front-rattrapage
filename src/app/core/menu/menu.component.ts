5581873b71e902fd4a6f3916556637d259bb7067
FuryPh4ntom <aurelien.serre04@gmail.com>
2025-06-26 23:30:47 +0200

    message non connection bdd

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Router, NavigationEnd } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { filter } from 'rxjs/operators';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, HttpClientModule, FormsModule],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  dbConnectionFailed = false;
+
   get isAdmin(): boolean {
     return localStorage.getItem('admin') === 'true';
   }
@@ -24,10 +27,24 @@ export class MenuComponent {
     return this.isUser || this.isAdmin;
   }
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {
+    this.checkApiConnection();
+  }
 
   logout() {
     localStorage.clear();
     this.router.navigate(['/connexion']);
+  }
+
+  checkApiConnection() {
+    // Exemple d’appel vers une route API simple
+    this.http.get('http://localhost:3000/api/key/current').subscribe({
+      next: () => {
+        this.dbConnectionFailed = false;
+      },
+      error: () => {
+        this.dbConnectionFailed = true;
+      }
+    });
   }
 }
